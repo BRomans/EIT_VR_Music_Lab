306aec2b03b1ba5c4bcf60a47eae405e62a41a9e
bromans <michele.romani.gzl0@gmail.com>
2020-02-27 16:35:18 +0100

    :sparkles: prefab for beats

---
+++ ARMusicLab/Assets/Scripts/BeatPlayer.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeatPlayer : MonoBehaviour
+{
+
+    [SerializeField] AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+     void OnTriggerEnter(Collider other)
+    {  
+        
+        if (other.gameObject.tag == "Beats")
+        {
+            audioSource.Play();
+        }
+
+    }
+
+    void OnTriggerExit() {
+        audioSource.Stop();
+    }
+}
